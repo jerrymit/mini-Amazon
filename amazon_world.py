c287a4d87d0bab2fceb5ee9c7beff0fa7423aa0c
James Huang <sh694@duke.edu>
2023-04-11 17:12:54 -0400

    amazon ups

--- amazon_world.py
+++ amazon_world.py
@@ -2,39 +2,88 @@ import socket
 from google.protobuf.internal.decoder import _DecodeVarint32
 from google.protobuf.internal.encoder import _EncodeVarint
 import world_amazon_pb2
+import amazon_ups_pb2
 
-# create a socket object
-amazon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# set the IP address and port number of the world server
-ip = '127.0.0.1'  # replace with the actual IP address of the world server
-port = 23456
+def amazon_ups_server():
+    # create a socket object
+    amazon_ups_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect to the world server
-amazon_socket.connect((ip, port))
+    # get local machine name
+    host = socket.gethostname()
 
-# create an AConnect message to send to the world server
-connect_msg = world_amazon_pb2.AConnect()
-connect_msg.isAmazon = True  # set the isAmazon field to True
+    # set the port for UPS to connect to
+    port = 34567
+    print("hostname", host)
 
-# encode the AConnect message and send it to the world server
-encoded_msg = connect_msg.SerializeToString()
-_EncodeVarint(amazon_socket.send, len(encoded_msg), None)
-amazon_socket.send(encoded_msg)
+    # bind the socket to a specific address and port
+    amazon_ups_socket.bind((host, port))
 
-# receive a message from the world server
-var_int_buff = []
-while True:
-    buf = amazon_socket.recv(1)
-    var_int_buff += buf
-    msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
-    if new_pos != 0:
-        break
-whole_msg = amazon_socket.recv(msg_len)
+    # listen for incoming connections
+    amazon_ups_socket.listen(5)
 
-# parse the received message as an AConnected message
-connected_msg = world_amazon_pb2.AConnected()
-connected_msg.ParseFromString(whole_msg)
+    # wait for a connection
+    print('Waiting for a connection...')
+    client_socket, addr = amazon_ups_socket.accept()
 
-# print the received message
-print(connected_msg)
+    # print the client address
+    print(f'Got a connection from {addr}')
+
+def amazon_world_client(amazon_ups_socket):
+    # receive a message from the UPS client
+    var_int_buff = []
+    while True:
+        buf = amazon_ups_socket.recv(1)
+        var_int_buff += buf
+        msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
+        if new_pos != 0:
+            break
+    whole_msg = amazon_ups_socket.recv(msg_len)
+
+    # parse the received message as an AConnected message
+    UToA_msg = amazon_ups_pb2.UtoAzConnect()
+    UToA_msg.ParseFromString(whole_msg)
+
+    # print the received message
+    print("Received message with worldid:", UToA_msg.worldid)
+
+
+    # create a socket object
+    amazon_world_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # set the IP address and port number of the world server
+    ip = '127.0.0.1'  # replace with the actual IP address of the world server
+    port = 23456
+
+    # connect to the world server
+    amazon_world_socket.connect((ip, port))
+
+    # create an AConnect message to send to the world server
+    connect_msg = world_amazon_pb2.AConnect()
+    connect_msg.isAmazon = True  # set the isAmazon field to True
+
+    # encode the AConnect message and send it to the world server
+    encoded_msg = connect_msg.SerializeToString()
+    _EncodeVarint(amazon_world_socket.send, len(encoded_msg), None)
+    amazon_world_socket.send(encoded_msg)
+
+    # receive a message from the world server
+    var_int_buff = []
+    while True:
+        buf = amazon_world_socket.recv(1)
+        var_int_buff += buf
+        msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
+        if new_pos != 0:
+            break
+    whole_msg = amazon_world_socket.recv(msg_len)
+
+    # parse the received message as an AConnected message
+    connected_msg = world_amazon_pb2.AConnected()
+    connected_msg.ParseFromString(whole_msg)
+
+    # print the received message
+    print(connected_msg)
+
+if __name__ == "__main__":
+    amazon_ups_socket = amazon_ups_server()
+    amazon_world_client(amazon_ups_socket)
