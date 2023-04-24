e7510c34c65312ad647d6a44e9abc4fb4d219e17
Jerry <dm0011662@gmail.com>
2023-04-24 14:31:04 -0400

    merge from James 0424

--- webApp/amazon_website/amazon/urls.py
+++ webApp/amazon_website/amazon/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('add_product', views.add_product, name='add_product'),
     path('add_warehouse', views.add_warehouse, name='add_warehouse'),
     path('cart', views.cart_items, name='cart'),
-<<<<<<< HEAD
     path('cart/clear/', views.clear_cart, name='clear_cart'),
     #path('buy_confirmed', views.buy_confirm, name='buy_confirmed'),
     path('Cartbuy', views.Cartbuy, name='Cartbuy'),
@@ -20,10 +19,5 @@ urlpatterns = [
     #path('Order_Status', views.package_detail, name='Order_Status'),
     path('order_status', views.order_status, name='order_status'),
     #path('Product_status', views.package_status, name='Product_status'),
-    
-=======
-    path('buy_confirmed/<int:order_id>', views.buy_confirm, name='buy_confirmed'),
-    path('Cartbuy', views.Cartbuy, name='Cartbuy'),
->>>>>>> master
     #path('home/search_results/order', views.assign_order, name='order'),
 ]
