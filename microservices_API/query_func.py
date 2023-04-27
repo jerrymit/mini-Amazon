58a488cd7151a89444da90ae2897bb06037eef44
Jerry <dm0011662@gmail.com>
2023-04-26 21:18:55 -0400

    cache

--- microservices_API/query_func.py
+++ microservices_API/query_func.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import random, socket, json
 from tables import *
 
-db_url = "postgresql://postgres:passw0rd@127.0.0.1:5432/amazon2"
+db_url = "postgresql://postgres:amazon@127.0.0.1:5432/amazon5"
 engine = create_engine(db_url)
 Session = sessionmaker(bind=engine)
 
