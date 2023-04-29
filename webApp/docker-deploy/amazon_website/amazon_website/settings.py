cb01dd3363770dd77063d3c4ea60bea0bbc5afe7
James Huang <sh694@duke.edu>
2023-04-29 19:40:33 -0400

    docker done?2

--- webApp/docker-deploy/amazon_website/amazon_website/settings.py
+++ webApp/docker-deploy/amazon_website/amazon_website/settings.py
@@ -85,28 +85,28 @@ WSGI_APPLICATION = 'amazon_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'amazon2',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD':'passw0rd',
-        'HOST': '127.0.0.1',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'amazon2',
+#         'USER': 'postgres',
+#         'PASSWORD':'passw0rd',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
